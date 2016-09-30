1e137a8674b266ee49449301fa06c766f6b0fcd6
Tomasz Madeyski <tomasz.madeyski@gmail.com>
2016-09-30 14:00:04 +0200

    test

--- index.php
+++ index.php
@@ -22,7 +22,9 @@
 
 
 //	Set base path here so this works with symbolic links for multiple installations
-
+echo getenv('MW_ELASTIC_URL') . PHP_EOL;
+echo getenv('MW_ELASTIC_PASSWORD') . PHP_EOL;
+die();
 	define('QA_BASE_DIR', dirname(empty($_SERVER['SCRIPT_FILENAME']) ? __FILE__ : $_SERVER['SCRIPT_FILENAME']).'/');
 
 	require 'qa-include/qa-index.php';
