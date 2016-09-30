688dfa5aeb583d920cc59bb51debca549194a828
Tomasz Madeyski <tomasz.madeyski@gmail.com>
2016-09-30 11:31:42 +0200

    test

--- index.php
+++ index.php
@@ -21,8 +21,6 @@
 */
 
 
-echo getenv('MYSQLCONNSTR_localdb');
-die();
 //	Set base path here so this works with symbolic links for multiple installations
 
 	define('QA_BASE_DIR', dirname(empty($_SERVER['SCRIPT_FILENAME']) ? __FILE__ : $_SERVER['SCRIPT_FILENAME']).'/');
